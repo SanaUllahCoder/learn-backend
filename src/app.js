c663551e4949fbf9c591dde383a0d11064e55d00
SanaUllahCoder <sanaullah.siddiqui0317@gmail.com>
2026-02-28 16:06:46 +0500

    learn delete and patch method

--- src/app.js
+++ src/app.js
@@ -10,17 +10,41 @@ app.use(express.json())
 // method post  and in the method data send
 app.post('/notes', (req, res) => {
     notes.push(req.body);
+    // console.log(req.body);
     res.status(201).json({
-        msg: "successfully created"
+        msg: "Notes fetched successfully"
     })
 });
 
+// GET METHOD  
 app.get('/notes', (req, res) => {
-    
-    res.status(201).json({
-        msg: "successfully created",
-        notes : notes
+    res.status(200).json({
+        msg: "Notes fetched successfully",
+        notes: notes
+    });
+});
+
+// DELETE METHOD 
+// app.delete('/notes/:index', (req, res) => {
+//     const index = req.params.index;
+//     delete notes[ index ];
+//     res.status(200).json({
+//         msg: "Notes deleted successfully"
+//     })
+
+// });
+
+// PATH METHOD 
+app.patch('/notes/:index', (req, res) => {
+
+    const index = req.params.index;
+    const names = req.body.name;
+    notes[ index ].name = names;
+
+    res.status(200).json({
+        msg: "Notes updated successfully"
     })
+
 })
 
 module.exports = app;
